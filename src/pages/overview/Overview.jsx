7f0742982efe6ea9045e72b5b87637dff40f18f4
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-21 11:29:08 +0600

    overview page responsive done

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -7,15 +7,11 @@ import CompareActivity from '../../components/overview/CompareActivity';
 const Overview = () => {
     return (
         <div className="py-4 px-2">
-                <div className="flex items-center gap-5 scrollbar-hide [&::-webkit-scrollbar]:hidden overflow-auto lg:grid grid-cols-2 ">
+                <div className="grid lg:grid-cols-2 gap-3">
                     <SalesActivity></SalesActivity>
                     <ProductsSummary></ProductsSummary>
                     <CustomerActivity></CustomerActivity>
                     <CompareActivity></CompareActivity>
-                </div>
-                <div className="lg:hidden mt-5 border h-10">
-                    <h1 className="text-sm font-semibold text-gray-700">Easy to Move</h1>
-
                 </div>
         </div>
     );
